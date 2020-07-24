fb6d9900024a17552668bbea93fc915a61e9ebf3
CristianD <dumitru.cristian98@yahoo.com>
2020-07-24 10:57:05 +0300

    Importing modules on auth

--- src/app/authentification/authentification.module.ts
+++ src/app/authentification/authentification.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [RegisterComponent],
   imports: [
+    RegisterComponent,
+    LoginComponent,
     CommonModule
   ]
 })
